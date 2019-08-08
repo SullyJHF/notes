58bd3fe408ea8c48be6925c85bc0ee46879683b7
Sullivan Ford <sullyjhford@gmail.com>
2019-08-08 16:19:10 +0100

    Working with ejs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import path from 'path';
 import express from 'express';
 
 const PORT = process.env.PORT || 3000;
 let app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use('/css', express.static(path.join(__dirname, '/css')));
+
+app.get('/', function(req, res) {
+    res.render('notes');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
