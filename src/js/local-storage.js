d025b8e26f2396ae566650e442b3c3ff3fba50b5
Sullivan Ford <sullyjhford@gmail.com>
2019-08-08 16:19:16 +0100

    Get note saving and loading from/to local storage

---
+++ src/js/local-storage.js
@@ -0,0 +1,9 @@
+export default class LocalStorage {
+  static setItem(key, value) {
+    localStorage.setItem(key, value);
+  }
+
+  static getItem(key) {
+    return localStorage.getItem(key);
+  }
+}
--- src/js/notes.js
+++ src/js/notes.js
@@ -1,3 +1,17 @@
 import apple from './apple';
+import LocalStorage from './local-storage';
 
-console.log('hello!');
+let textarea = document.getElementById('notes');
+textarea.value = LocalStorage.getItem('note');
+
+textarea.onkeyup = (e) => {
+  if (e.ctrlKey || e.metaKey || e.shiftKey || e.altKey) return;
+  LocalStorage.setItem('note', textarea.value);
+};
+
+textarea.onkeydown = (e) => {
+  if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+    e.preventDefault();
+    LocalStorage.setItem('note', textarea.value);
+  }
+}
